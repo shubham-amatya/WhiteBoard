7e8a958f97bad7b72c0e49557a7b6a3717be7610
shubham-amatya <72518585+shubham-amatya@users.noreply.github.com>
2021-01-18 23:14:14 -0500

    Delete TheCopperPot directory

--- TheCopperPot/src/main/java/com/demo/TheCopperPot/Repositories/StoreRepository.java
+++
@@ -1,9 +0,0 @@
-package com.demo.TheCopperPot.Repositories;
-
-import org.springframework.data.repository.CrudRepository;
-import com.demo.TheCopperPot.Models.Store;
-
-public interface StoreRepository extends CrudRepository<Store, Integer> {
-
-
-}
